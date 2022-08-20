f11bc99af0319a8757e5963309431c758140e089
Arthur Lapz <rlapz@gnuweeb.org>
2022-08-21 00:11:50 +0700

    Url: Avoid unnecessary copying encoded text twice
    Signed-off-by: Arthur Lapz <rlapz@gnuweeb.org>

--- src/Url.zig
+++ src/Url.zig
@@ -17,46 +17,12 @@ const protocol   = "HTTP/1.1";
 const query      = "/translate_a/single?client=gtx&ie=UTF-8&oe=UTF-8";
 const user_agent = "Mozilla/5.0";
 const connection = "Close";
-const req_format = "{s} {s} {s}\r\n" ++
-                   "Host: {s}\r\n" ++
-                   "User-Agent: {s}\r\n" ++
-                   "Connection: {s}\r\n\r\n";
 
-// 0: source lang
-// 1: target lang
-// 2: text
-const brief = std.fmt.comptimePrint(
-    req_format,
-    .{
-        method, query ++ "&dt=t&sl={s}&tl={s}&q={s}", protocol,
-        host, user_agent, connection
-    }
-);
-
-// 0: source lang
-// 1: target lang
-// 2: hint lang
-// 3: text
-const detail = std.fmt.comptimePrint(
-    req_format,
-    .{
-        method,
-        query ++ "&dt=bd&dt=ex&dt=ld&dt=md&dt=rw&dt=rm&dt=ss&dt=" ++
-                 "t&dt=at&dt=gt&dt=qca&sl={s}&tl={s}&hl={s}&q={s}",
-        protocol,
-        host, user_agent, connection
-    }
-);
-
-// 0: text
-const det_lang = std.fmt.comptimePrint(
-    req_format,
-    .{
-        method, query ++ "&sl=auto&q={s}", protocol,
-        host, user_agent, connection
-    }
-);
-
+const brief      = "&dt=t";
+const detail     = "&dt=bd&dt=ex&dt=ld&dt=md&dt=rw&dt=rm&dt=ss&dt=" ++
+                   "t&dt=at&dt=gt&dt=qca";
+const det_lang   = "";
+// zig fmt: on
 
 pub const UrlBuildType = enum(u32) {
     brief = 1,
@@ -64,22 +30,20 @@ pub const UrlBuildType = enum(u32) {
     detect_lang,
 
     pub fn str(self: @This()) []const u8 {
-        return switch(self) {
-            .brief       => "Brief",
-            .detail      => "Detail",
+        return switch (self) {
+            .brief => "Brief",
+            .detail => "Detail",
             .detect_lang => "Detect Language",
-
         };
     }
 };
 
-
 pub fn build(
-    buffer  : []u8,
+    buffer: []u8,
     url_type: UrlBuildType,
     src_lang: []const u8,
     trg_lang: []const u8,
-    text    : []const u8,
+    text: []const u8,
 ) Error![]const u8 {
     // At least the buffer length should be three times larger than
     // the text length,
@@ -87,30 +51,36 @@ pub fn build(
     if (buffer.len <= (text.len * 3))
         return Error.NoSpaceLeft;
 
-    const text_enc = encode(buffer, text);
-
-    return switch (url_type) {
-        .brief => std.fmt.bufPrint(
-            buffer[text_enc.len..], brief,
-            .{
-                src_lang, trg_lang, text_enc
-            }
+    // zig fmt: on
+    var ret = switch (url_type) {
+        .brief => try std.fmt.bufPrint(
+            buffer,
+            "{s} {s}" ++ brief ++ "&sl={s}&tl={s}&q=",
+            .{ method, query, src_lang, trg_lang },
         ),
-        .detail => std.fmt.bufPrint(
-            buffer[text_enc.len..], detail,
-            .{
-                src_lang, trg_lang, trg_lang, text_enc
-            }
+        .detail => try std.fmt.bufPrint(
+            buffer,
+            "{s} {s}" ++ detail ++ "&sl={s}&tl={s}&hl={s}&q=",
+            .{ method, query, src_lang, trg_lang, trg_lang },
         ),
-        .detect_lang => std.fmt.bufPrint(
-            buffer[text_enc.len..], det_lang,
-            .{
-                text_enc
-            }
+        .detect_lang => try std.fmt.bufPrint(
+            buffer,
+            "{s} {s}" ++ det_lang ++ "&sl={s}&q=",
+            .{ method, query, "auto" },
         ),
     };
+
+    const text_enc = encode(buffer[ret.len..], text);
+    var len = ret.len + text_enc.len;
+
+    ret = try std.fmt.bufPrint(
+        buffer[len..],
+        " {s}\r\nHost: {s}\r\nUser-Agent: {s}\r\nConnection: {s}\r\n\r\n",
+        .{ protocol, host, user_agent, connection },
+    );
+
+    return buffer[0 .. len + ret.len];
 }
-// zig fmt: on
 
 fn encode(dest: []u8, src: []const u8) []const u8 {
     const hex = "0123456789abcdef";
