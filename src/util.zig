170a0a9753e0cd9c0e8b446e6df3e566eeeab009
Arthur Lapz <rlapz@gnuweeb.org>
2022-08-23 19:21:47 +0700

    util: Using multidimensional array instead of struct
    Signed-off-by: Arthur Lapz <rlapz@gnuweeb.org>

--- src/util.zig
+++ src/util.zig
@@ -7,23 +7,18 @@
 const std = @import("std");
 
 pub fn skipHtmlTags(dest: []u8, src: []const u8) []const u8 {
-    const Table = struct { tags: [2][]const u8 };
-
-    const tables = [_]Table{
-        .{ .tags = .{ "<b>", "</b>" } },
+    const tables = [_][2][]const u8{
+        .{ "<b>", "</b>" },
         // Add other tags below
     };
 
     var ret = dest;
-    for (tables) |*v| {
-        const tag0 = v.tags[0];
-        const tag1 = v.tags[1];
+    for (tables) |*tag| {
+        const op = std.mem.replace(u8, src, tag[0], "", dest);
+        ret = dest[0 .. src.len - (op * tag[0].len)];
 
-        const op = std.mem.replace(u8, src, tag0, "", dest);
-        ret = dest[0 .. src.len - (op * tag0.len)];
-
-        const cl = std.mem.replace(u8, ret, tag1, "", ret);
-        ret = dest[0 .. ret.len - (cl * tag1.len)];
+        const cl = std.mem.replace(u8, ret, tag[1], "", ret);
+        ret = dest[0 .. ret.len - (cl * tag[1].len)];
     }
 
     return ret;
