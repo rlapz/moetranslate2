c0e1db120f96a9b54ef9e872473c4e22ef03783d
Arthur Lapz <rlapz@gnuweeb.org>
2022-08-23 19:25:30 +0700

    util: Fix skipHtmlTags cannot skips multiple tags
    Signed-off-by: Arthur Lapz <rlapz@gnuweeb.org>

--- src/util.zig
+++ src/util.zig
@@ -12,13 +12,15 @@ pub fn skipHtmlTags(dest: []u8, src: []const u8) []const u8 {
         // Add other tags below
     };
 
+    var _src = src;
     var ret = dest;
     for (tables) |*tag| {
-        const op = std.mem.replace(u8, src, tag[0], "", dest);
-        ret = dest[0 .. src.len - (op * tag[0].len)];
+        const op = std.mem.replace(u8, _src, tag[0], "", dest);
+        ret = dest[0 .. _src.len - (op * tag[0].len)];
 
         const cl = std.mem.replace(u8, ret, tag[1], "", ret);
         ret = dest[0 .. ret.len - (cl * tag[1].len)];
+        _src = ret;
     }
 
     return ret;
